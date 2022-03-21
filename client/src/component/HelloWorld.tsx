7db8fd2cbfc8476d1b6005a99b76fff70cfafcda
SEBFull-Jeongmin <lee.jeongmin1625@gmail.com>
2022-03-21 14:52:12 +0900

    helloworld
    helloworld 받기

--- client/src/component/HelloWorld.tsx
+++ client/src/component/HelloWorld.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import axios from "axios";
 
 const HelloWorld = () => {
-  // axios
-  //   .get("서버주소", { withCredentials: false })
-  //   .then(() => console.log("Hello World!"));
+  axios.get(
+    "http://ec2-13-125-241-113.ap-northeast-2.compute.amazonaws.com"
+  )
+  .then((res)=>console.log(res))
+
   return <div>Hello World!</div>;
 };
 
