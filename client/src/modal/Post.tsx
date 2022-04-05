9fdd4de0bf47d44c9f90e1b3834474e8a4b44e33
mh11kim <91522505+mh11kim@users.noreply.github.com>
2022-04-05 19:52:44 +0900

    Update Post.tsx

--- client/src/modal/Post.tsx
+++ client/src/modal/Post.tsx
@@ -6,22 +6,18 @@ import store from "../redux/Store";
 
 export type RootState = ReturnType<typeof store.getState>;
 
-type Props = {
-  handlePostModal: () => void;
-  address: [string];
-};
+const Post = (props: any) => {
 
-const Post = ({ handlePostModal, address }: Props) => {
   const navigate = useNavigate();
   // const localId = useSelector((localId: RootState) => localId.Reducer.id);
   //서버에서 user id를 redux에 저장한 것을 여기로 꺼내오기.
   const localStorageTokenCheck: any = localStorage.getItem("KEY");
   const post = useSelector((data: RootState) => data.restaurantEnrollment);
-
+    // console.log(post.name);
   //!!!!!!!!!!!!!!   //!!!!!!!!!!!!!!!!!!!!!!!!!!!!   //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-  const uri = address;
-  console.log(uri);
+  const uri = props.address;
+  console.log(uri)
   const uploadPicutre = () => {
     if (uri) {
       axios.post(
@@ -62,7 +58,7 @@ const Post = ({ handlePostModal, address }: Props) => {
         .then(() => navigate("/restaurant"))
         // //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         .then(() => uploadPicutre());
-      //35번줄 조건에 사진을 넣어햐하나???
+        //35번줄 조건에 사진을 넣어햐하나???
       //!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     } else {
       alert("상호명과 주소는 필수입니다.");
@@ -73,7 +69,7 @@ const Post = ({ handlePostModal, address }: Props) => {
     <div>
       <div>포스트를 등록하시겠습니까?</div>
       <button onClick={handlePost}>예</button>
-      <button onClick={handlePostModal}>취소</button>
+      <button onClick={props.handlePostModal}>취소</button>
     </div>
   );
 };
